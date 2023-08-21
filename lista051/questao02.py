915c44e7e170989b5e0cb9215886b0b9be47e70a
Lucas Belkior <lucasbelkior9@gmail.com>
2023-08-21 11:42:06 -0300

    lacos

---
+++ lista051/questao02.py
@@ -0,0 +1,9 @@
+'''
+ Desenvolver um programa que exiba os números em ordem decrescente de 100 até 1.
+'''
+
+contador = 100
+
+while ( contador >= 1 ):
+    print( contador )
+    contador = contador - 1
---
+++ lista051/questao03.py
@@ -0,0 +1,12 @@
+'''
+ Desenvolver um programa que apresente os quadrados dos números inteiros de 15 a 200.
+'''
+
+contador = 15
+while ( contador <= 200 ):
+    print( contador*contador )
+    contador = contador + 1
+
+
+
+
